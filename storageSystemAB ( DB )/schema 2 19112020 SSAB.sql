8fccaf1dae90c608e4e6beebdfb91d79c6cdc20d
Ahmed-Eid12 <ahmedzidan757@gmail.com>
2020-11-19 11:03:33 +0200

    upload store system schema backup with pic

---
+++ storageSystemAB ( DB )/schema 2 19112020 SSAB.sql
@@ -0,0 +1,356 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: storagesystem
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bill_products`
+--
+
+DROP TABLE IF EXISTS `bill_products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bill_products` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `quantity` int DEFAULT NULL,
+  `bill_id` int DEFAULT NULL,
+  `category_id` int DEFAULT NULL,
+  `notes` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_billProduct_bill_idx` (`bill_id`),
+  KEY `fk_category_idx` (`category_id`),
+  CONSTRAINT `fk_billProduct_bill` FOREIGN KEY (`bill_id`) REFERENCES `bills` (`id`),
+  CONSTRAINT `fk_category` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bill_products`
+--
+
+LOCK TABLES `bill_products` WRITE;
+/*!40000 ALTER TABLE `bill_products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bill_products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bill_type`
+--
+
+DROP TABLE IF EXISTS `bill_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bill_type` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `decription` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bill_type`
+--
+
+LOCK TABLES `bill_type` WRITE;
+/*!40000 ALTER TABLE `bill_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bill_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bills`
+--
+
+DROP TABLE IF EXISTS `bills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bills` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `code_generation` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `incoming_company_id` int DEFAULT NULL,
+  `store_id` int DEFAULT NULL,
+  `bill_type_id` int DEFAULT NULL,
+  `project_id` int DEFAULT NULL,
+  `store_id_to` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_incoming_company_idx` (`incoming_company_id`),
+  KEY `fk_storeId_idx` (`store_id`),
+  KEY `fk_bill_type_idx` (`bill_type_id`),
+  KEY `fk_proId_idx` (`project_id`),
+  KEY `fk_storeIdTo_idx` (`store_id_to`),
+  CONSTRAINT `fk_bill_type` FOREIGN KEY (`bill_type_id`) REFERENCES `bill_type` (`id`),
+  CONSTRAINT `fk_incoming_company` FOREIGN KEY (`incoming_company_id`) REFERENCES `incoming_companies` (`id`),
+  CONSTRAINT `fk_proId` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`),
+  CONSTRAINT `fk_storeId` FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`),
+  CONSTRAINT `fk_storeIdTo` FOREIGN KEY (`store_id_to`) REFERENCES `stores` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bills`
+--
+
+LOCK TABLES `bills` WRITE;
+/*!40000 ALTER TABLE `bills` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bills` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `decription` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `unit_id` int DEFAULT NULL,
+  `store_type_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_unit_idx` (`unit_id`),
+  KEY `fk_store_type_idx` (`store_type_id`),
+  CONSTRAINT `fk_store_type` FOREIGN KEY (`store_type_id`) REFERENCES `store_type` (`id`),
+  CONSTRAINT `fk_unit` FOREIGN KEY (`unit_id`) REFERENCES `units` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `incoming_companies`
+--
+
+DROP TABLE IF EXISTS `incoming_companies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `incoming_companies` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `decription` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `address` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `phone` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `incoming_companies`
+--
+
+LOCK TABLES `incoming_companies` WRITE;
+/*!40000 ALTER TABLE `incoming_companies` DISABLE KEYS */;
+/*!40000 ALTER TABLE `incoming_companies` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projects`
+--
+
+DROP TABLE IF EXISTS `projects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `projects` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `decription` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `address` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `store_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_pro_store_idx` (`store_id`),
+  CONSTRAINT `fk_pro_store` FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `projects`
+--
+
+LOCK TABLES `projects` WRITE;
+/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `decription` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `store_type`
+--
+
+DROP TABLE IF EXISTS `store_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `store_type` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `decription` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `location` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `store_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_store_idx` (`store_id`),
+  CONSTRAINT `fk_store` FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `store_type`
+--
+
+LOCK TABLES `store_type` WRITE;
+/*!40000 ALTER TABLE `store_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `store_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stores`
+--
+
+DROP TABLE IF EXISTS `stores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stores` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `decription` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `location` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_userId_idx` (`user_id`),
+  CONSTRAINT `fk_userId` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stores`
+--
+
+LOCK TABLES `stores` WRITE;
+/*!40000 ALTER TABLE `stores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sys_gender`
+--
+
+DROP TABLE IF EXISTS `sys_gender`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sys_gender` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `gender` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_gender`
+--
+
+LOCK TABLES `sys_gender` WRITE;
+/*!40000 ALTER TABLE `sys_gender` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sys_gender` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `units`
+--
+
+DROP TABLE IF EXISTS `units`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `units` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `decription` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `units`
+--
+
+LOCK TABLES `units` WRITE;
+/*!40000 ALTER TABLE `units` DISABLE KEYS */;
+/*!40000 ALTER TABLE `units` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `code` mediumtext,
+  `user_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(150) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `address` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `phone` int DEFAULT NULL,
+  `role_id` int DEFAULT NULL,
+  `date_modify` datetime DEFAULT NULL,
+  `modified_by` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_role_idx` (`role_id`),
+  CONSTRAINT `fk_role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-19 10:57:03
--- storageSystemAB ( DB )/storageSystem.sql
+++ storageSystemAB ( DB )/storageSystem.sql
@@ -12,12 +12,132 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `code` long,
-  `is_admin` int(11) default 0,
-  `user_name` varchar(50) NOT NULL ,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(150) NOT NULL,
+  `user_name` nvarchar(50) NOT NULL ,
+  `email` nvarchar(50) NOT NULL,
+  `password` nvarchar(150) NOT NULL,
+  `address` nvarchar(300) ,
+  `phone` int(15),
+  `role_id` int(11),
   `date_modify` datetime ,
   `modified_by` int(11) ,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+# ------------------------------------------------------------------------
+DROP TABLE IF EXISTS `roles`;
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `decription` nvarchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+# ------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `units`;
+
+CREATE TABLE `units` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `decription` nvarchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+# ------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `stores`;
+
+CREATE TABLE `stores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `decription` nvarchar(100) NOT NULL,
+  `location` nvarchar(200),
+  `user_id` int(11),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+# ------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `store_type`;
+
+CREATE TABLE `store_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `decription` nvarchar(100) NOT NULL,
+  `location` nvarchar(200),
+  `store_id` int(11),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+# ------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `categories`;
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `decription` nvarchar(100) NOT NULL,
+  `unit_id` int(11),
+  `store_type_id` int(11),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+# ------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `projects`;
+
+CREATE TABLE `projects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `decription` nvarchar(100) NOT NULL,
+  `address` nvarchar(200),
+  `user_id` int(11),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+# ------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `incoming_companies`;
+
+CREATE TABLE `incoming_companies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `decription` nvarchar(100) NOT NULL,
+  `address` nvarchar(200),
+  `phone` int(15),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+# ------------------------------------------------------------------------
+# الاذنات
+
+DROP TABLE IF EXISTS `bill_type`;
+
+CREATE TABLE `bill_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `decription` nvarchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+# ------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `bills`;
+
+CREATE TABLE `bills` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code_generation` nvarchar(50),
+  `incoming_company_id` int(11),
+  `store_id` int(11),
+  `bill_type_id` int(15),
+  `project_id` int(15),
+  `store_id_to` int(15),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+# ------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `bill_products`;
+
+CREATE TABLE `bill_products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `quantity` int(11),
+  `bill_id` int(11),
+  `category_id` int(11),
+  `notes` nvarchar(300),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
@@ -27,8 +147,7 @@ DROP TABLE IF EXISTS `sys_gender`;
 
 CREATE TABLE `sys_gender` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `code` long ,
-  `gender` varchar(45) NOT NULL,
+  `gender` nvarchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
